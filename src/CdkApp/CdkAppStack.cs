b84875449169bfa1d225298f96a5e2ff1db59104
collin-smith <74462915+collin-smith@users.noreply.github.com>
2021-01-17 08:43:28 -0700

    Initial commit

---
+++ src/CdkApp/CdkAppStack.cs
@@ -0,0 +1,12 @@
+using Amazon.CDK;
+
+namespace CdkApp
+{
+    public class CdkAppStack : Stack
+    {
+        internal CdkAppStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
